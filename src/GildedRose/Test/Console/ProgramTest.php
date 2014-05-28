08b01dd5f879ec5e2a339d4218c73f5a9b69168a
Christian Soronellas <christian.soronellas@atrapalo.com>
2014-05-28 13:05:03 +0200

    First commit

---
+++ src/GildedRose/Test/Console/ProgramTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace GildedRose\Test\Console;
+
+use PHPUnit_Framework_TestCase;
+
+class ProgramTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function theTruth()
+    {
+        $this->assertTrue(true);
+    }
+}
