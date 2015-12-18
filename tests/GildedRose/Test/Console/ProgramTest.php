555989b005e7e5f9545778d2a303fd72dbe36daa
Christian Soronellas <christian.soronellas@atrapalo.com>
2015-12-18 15:59:08 +0100

    Added a functional test that covers the whole program

--- tests/GildedRose/Test/Console/ProgramTest.php
+++ tests/GildedRose/Test/Console/ProgramTest.php
@@ -2,6 +2,7 @@
 
 namespace GildedRose\Test\Console;
 
+use GildedRose\Console\Program;
 use PHPUnit_Framework_TestCase;
 
 class ProgramTest extends PHPUnit_Framework_TestCase
@@ -9,8 +10,29 @@ class ProgramTest extends PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function theTruth()
+    public function it_should_display_the_correct_output()
     {
-        $this->assertTrue(true);
+        $this->expectOutputString(<<<EOO
+OMGHAI!
+                                              Name -  SellIn - Quality
+                                 +5 Dexterity Vest -       9 -      19
+                                         Aged Brie -       1 -       1
+                                         Aged Brie -      -2 -       2
+                            Elixir of the Mongoose -       4 -       6
+                            Elixir of the Mongoose -      -2 -       5
+                        Sulfuras, Hand of Ragnaros -       0 -      80
+                        Sulfuras, Hand of Ragnaros -      -1 -      80
+         Backstage passes to a TAFKAL80ETC concert -      14 -      21
+         Backstage passes to a TAFKAL80ETC concert -       9 -      50
+         Backstage passes to a TAFKAL80ETC concert -       4 -      50
+         Backstage passes to a TAFKAL80ETC concert -       4 -      50
+         Backstage passes to a TAFKAL80ETC concert -       4 -      13
+         Backstage passes to a TAFKAL80ETC concert -      -2 -       0
+                                Conjured Mana Cake -       2 -       5
+
+EOO
+        );
+
+        Program::main();
     }
 }
