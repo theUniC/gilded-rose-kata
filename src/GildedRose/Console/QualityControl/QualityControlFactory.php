0a6ba68e991c7f93dda0deb0a54ea4f2f90957c1
Christian Soronellas <christian.soronellas@atrapalo.com>
2015-12-19 13:26:50 +0100

    Updated QualityControlFactory and Program's unit test

--- src/GildedRose/Console/QualityControl/QualityControlFactory.php
+++ src/GildedRose/Console/QualityControl/QualityControlFactory.php
@@ -8,6 +8,7 @@ class QualityControlFactory
 {
     const AGED_BRIE         = "Aged Brie";
     const BACKSTAGE_PASSES  = "Backstage passes to a TAFKAL80ETC concert";
+    const CONJURED          = "Conjured Mana Cake";
 
     public function qualityControlFor(Item $item)
     {
@@ -17,6 +18,10 @@ class QualityControlFactory
 
         if ($this->is(self::BACKSTAGE_PASSES, $item)) {
             return new BackstagePassesQualityControl();
+        }
+
+        if ($this->is(self::CONJURED, $item)) {
+            return new ConjuredQualityControl();
         }
 
         return new DefaultQualityControl();
--- tests/GildedRose/Test/Console/ProgramTest.php
+++ tests/GildedRose/Test/Console/ProgramTest.php
@@ -25,14 +25,18 @@ class ProgramTest extends PHPUnit_Framework_TestCase
     /** @test */
     public function it_should_sell_non_sulfuras_items()
     {
-        $agedBrie = new Item(['name' => "Aged Brie", "sellIn" => 2, 'quality' => 10]);
-        $backstage = new Item(['name' => "Backstage passes to a TAFKAL80ETC concert", 'sellIn' => 15, 'quality' => 20]);
+        $agedBrie   = new Item(['name' => "Aged Brie", "sellIn" => 2, 'quality' => 10]);
+        $backstage  = new Item(['name' => "Backstage passes to a TAFKAL80ETC concert", 'sellIn' => 15, 'quality' => 20]);
+        $conjured   = new Item(['name' => 'Conjured Mana Cake','sellIn' => 3,'quality' => 6]);
+        $default    = new Item(['name' => '+5 Dexterity Vest', 'sellIn' => 10, 'quality' => 20]);
 
-        $app = new Program([$agedBrie, $backstage]);
+        $app = new Program([$agedBrie, $backstage, $conjured, $default]);
 
         $app->UpdateQuality();
 
-        $this->assertEquals(1, $agedBrie->sellIn);
-        $this->assertEquals(14, $backstage->sellIn);
+        $this->assertEquals(1,      $agedBrie->sellIn);
+        $this->assertEquals(14,     $backstage->sellIn);
+        $this->assertEquals(2,      $conjured->sellIn);
+        $this->assertEquals(9,      $default->sellIn);
     }
 }
