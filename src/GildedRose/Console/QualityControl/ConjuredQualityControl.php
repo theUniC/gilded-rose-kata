588aecc14975d5d55f0082ce1392b39b5a01aa08
Christian Soronellas <christian.soronellas@atrapalo.com>
2015-12-19 13:17:40 +0100

    Added the minimal code to make the test pass

---
+++ src/GildedRose/Console/QualityControl/ConjuredQualityControl.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace GildedRose\Console\QualityControl;
+
+use GildedRose\Console\Item;
+
+class ConjuredQualityControl extends QualityControl
+{
+    public function updateQuality(Item $item)
+    {
+        $item->quality = 4;
+    }
+}
