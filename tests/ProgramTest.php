99366a082cf59adb7bdc4a45a78b41b155169575
theUniC <theunic@gmail.com>
2020-10-18 16:26:41 +0200

    Modernize code

---
+++ tests/ProgramTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\GildedRose\Console;
+
+use PHPUnit\Framework\TestCase;
+
+class ProgramTest extends TestCase
+{
+    /** @test */
+    public function theTruth(): void
+    {
+        $this->assertTrue(true);
+    }
+}
